75a72032a397a87c7e3ab8913b4ca6ff18248eb9
Sunny kr <119757134+mailsunnyhere-ON-GIT@users.noreply.github.com>
2024-02-22 11:33:04 +0530

    patch

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let randomNumber=parseInt(Math.random()*100+1)
-console.log(randomNumber)
 let submit=document.getElementById("butt")
 let prevGuess=document.getElementById("prevguess")
 let remGuess=document.getElementById("lastres")
@@ -8,68 +7,84 @@ let adding=document.getElementById("loworhigh")
 
 let start=true;
 let guessnum=1;
+console.log(randomNumber)
 
-
-if(true)
-{
+if(true){
     submit.addEventListener("click",(e)=>{
         e.preventDefault()
-
         let guess=parseInt(submission.value)
         validateInput(guess)
-       
     })
-
 }
-function validateInput(guess)
-{
-    if(isNaN(guess))
-    {
+
+function validateInput(guess){
+    if(isNaN(guess)){
         display("not a number")
     }
-    else if(guess<1)
-    {
+    else if(guess<1){
         display("Enter greater number")
     }
-    else if(100<guess)
-    {
+    else if(100<guess){
         display("Enter smaller number")
     }
     else{
         displayArray(guess)
         if(guessnum===11){
-        display(`game over , number was ${randomNumber}`)
-        endgame()    
-    }
+            display(`game over , number was ${randomNumber}`)
+            endgame()    
+        }
         else{
-           checkguess(guess)
+            checkguess(guess)
         }
     }
 }
-function checkguess(guess)
-{
-    if(randomNumber==guess)
-    {
+
+function checkguess(guess){
+    if(randomNumber==guess){
         display("u won")
         endgame()
     }
-    else if(randomNumber<guess)
-    {
+    else if(randomNumber>guess){
         display("guess low")
     }
     else{
         display("guess high")
     }
-
 }
-function display(msg)
-{
+
+function display(msg){
+    console.log(msg)
     adding.innerHTML=`${msg}`
 }
-function displayArray(guess)
-{
-    submit.innerHTML=""
+
+function displayArray(guess){
+    submission.value=""
     prevGuess.innerHTML+=`${guess} `
     guessnum++
     remGuess.innerHTML=`${11-guessnum}`
+}
+
+function endgame(){
+    start=false;
+    submit.setAttribute("disabled", "");
+    newgame()
+}
+
+function newgame(){
+    let reset = document.createElement("div"); 
+    let newGameText = document.createElement("span"); 
+    newGameText.textContent = "new game"; 
+    reset.appendChild(newGameText);
+    adding.appendChild(reset);
+    reset.addEventListener("click",(e)=>{
+        e.preventDefault()
+        start=true
+        submit.removeAttribute("disabled", "");
+        randomNumber=parseInt(Math.random()*100+1)
+        guessnum=1
+        prevGuess.innerHTML=''
+        remGuess.innerHTML=" "
+        adding.removeChild(reset)
+        adding.innerHTML=" "
+    })   
 }
